990724e5593a1877d1cb0834ba1d4fe3a1534df4
user <kyronko@naver.com>
2019-05-11 15:58:14 +0900

    버튼 이용해 실제로 서버에서 데이터 받아오기 실행

--- app/src/main/java/com/tjedit/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/tjedit/banklistfromserver/MainActivity.java
@@ -3,8 +3,10 @@ package com.tjedit.banklistfromserver;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tjedit.banklistfromserver.databinding.ActivityMainBinding;
+import com.tjedit.banklistfromserver.utils.ConnectServer;
 
 public class MainActivity extends BaseActivity {
     ActivityMainBinding act;
@@ -16,7 +18,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+    act.serverBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ConnectServer.getRequestInfoBank(mContext,null);
+        }
+    });
     }
 
     @Override
