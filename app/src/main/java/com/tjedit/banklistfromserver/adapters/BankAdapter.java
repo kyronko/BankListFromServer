bf97e9e04babd1da616e88b63192f845740ac775
user <kyronko@naver.com>
2019-05-12 11:32:02 +0900

    어댑터작성완료  - 은행 데이터 매칭하기

--- app/src/main/java/com/tjedit/banklistfromserver/adapters/BankAdapter.java
+++ app/src/main/java/com/tjedit/banklistfromserver/adapters/BankAdapter.java
@@ -2,8 +2,13 @@ package com.tjedit.banklistfromserver.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.tjedit.banklistfromserver.R;
 import com.tjedit.banklistfromserver.datas.Bank;
 
@@ -22,5 +27,25 @@ public class BankAdapter extends ArrayAdapter<Bank> {
         inf = LayoutInflater.from(mContext);
     }
 
+
+    @Override
+    public View getView(int position,  View convertView,   ViewGroup parent) {
+         View row = convertView;
+    if(row==null)
+    {
+        row= inf.inflate(R.layout.bank_list_item,null);
+    }
+    Bank bankData = mList.get(position);
+
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView bankNameTxt = row.findViewById(R.id.bankNameTxt);
+        TextView bankCodeTxt = row.findViewById(R.id.bankCodeTxt);
+
+        bankNameTxt.setText(bankData.name);
+        bankCodeTxt.setText(String.format("%s",bankData.code));
+
+        Glide.with(mContext).load(bankData.logo).into(logoImg);
+         return  row;
+    }
 
 }
